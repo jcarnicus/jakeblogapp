401ce12a1fa631aad2b73935a9f0483882fa42bc
Jake <jcarni@gmail.com>
2015-06-19 02:05:38 +0000

    try 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,23 @@
 class Post < ActiveRecord::Base
-validates :title, presence: true
-def instance_report
-    puts "Post title is #{title}"
-    puts "This is a great blog post, you should read it"
-end
+    
+    validates :title, presence: true
+    def instance_report
+        puts "Post title is #{title}"
+        puts "This is a great blog post, you should read it"
+    end
 
-def twitter_check
-    post_length = post.length
-    if post_length < 140
-    puts "Your msg is ready for Twitter"
-else 
-    puts "Message is too long"
-end
-end
-
-
-def self.class_report
-    puts "BOOM CLASS METHOD!"
-    puts "I have #{Post.count} posts available"
-end
+    #twitter check operates on the instance's title
+    def twitter_check
+        if  title.length < 140
+            puts "Your msg is ready for Twitter"
+        else 
+            puts "Message is too long"
+        end
+    end
+    
+    
+    def self.class_report
+        puts "BOOM CLASS METHOD!"
+        puts "I have #{Post.count} posts available"
+    end
 end
