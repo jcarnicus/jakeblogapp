76f55e017c61523967281180a764b2a2007f5ff5
Jake <jcarni@gmail.com>
2015-06-20 18:34:07 +0000

    adding show feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+    def index
+        render "index.html"
+    end
+
+    def show
+        @post = Post.first
+        render :show
+    end
+
 end
+
