5b372502f42a6b14ef82d8dc6f1426ee44ea53e9
Jake <jcarni@gmail.com>
2015-06-20 22:02:18 +0000

    Installed various gems, made create controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,19 @@ class PostsController < ApplicationController
        @post = Post.find( params["id"])
         render :show
     end
-
+# CREATE new posts
+    def new
+        @post = Post.new
+        render "new.html.erb"
+    end
+    
+    def create
+        @post = Post.create(post_params)
+            redirect_to post_path(@post)
+    end
   
+    def post_params
+        return params.require(:post).permit(:title, :body)
+        
+    end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
     root to: 'welcome#home'
     
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new, :create]
 end
 
 
